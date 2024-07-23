6eede5c7f1d700a03a69e78b626d7ce6ff2445dc
Isidro Leonyork <139909934+izzyBdev@users.noreply.github.com>
2024-07-23 17:15:54 -0400

    Base React Project : Startup code

---
+++ rev-learn/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+
+      <div>
+        <h2>Welcome to the RevLearn Application for Revature Project 2</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
