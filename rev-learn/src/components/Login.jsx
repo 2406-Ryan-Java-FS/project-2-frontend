24dccef6d650f83c5aae960c64c5d001ecbcda9a
mrCoutureLocal <>
2024-07-24 12:14:59 -0400

    Stubbed in login and register forms

--- rev-learn/src/components/Login.jsx
+++ rev-learn/src/components/Login.jsx
@@ -1,5 +1,14 @@
 
 export default function Login()
 {
-    return(<>Login</>)
+return(<>
+<center>
+<table>
+    <tr><td></td><td><h3>RevLearn Login</h3></td></tr>
+    <tr><td>Email</td><td><input type="text"/></td></tr>
+    <tr><td>Password</td><td><input type="password"/></td></tr>
+    <tr><td></td><td><button>Sign Up</button><button>Sign In</button></td></tr>
+</table>
+</center>
+</>)
 }
--- rev-learn/src/components/Register.jsx
+++ rev-learn/src/components/Register.jsx
@@ -2,8 +2,9 @@
 export default function Register()
 {
 return(<>
+<center>
 <table>
-    <tr><td>RevLearn</td><td></td></tr>
+    <tr><td></td><td><h3>RevLearn</h3></td></tr>
     <tr><td>First Name</td><td><input type="text"/></td></tr>
     <tr><td>Last Name</td><td><input type="text"/></td></tr>
     <tr><td>Email</td><td><input type="text"/></td></tr>
@@ -11,6 +12,6 @@ return(<>
     <tr><td>Password Confirm</td><td><input type="password"/></td></tr>
     <tr><td></td><td><button>Sign up!</button></td></tr>
 </table>
-
+</center>
 </>)
 }
