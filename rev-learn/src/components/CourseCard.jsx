93e5482ca66095ae5bdc4c37aef5256730e255e9
Scruffy pc <cayabyabrr@yahoo.com>
2024-07-25 14:59:13 -0700

    Pass student props into courseCard

--- rev-learn/src/components/CourseCard.jsx
+++ rev-learn/src/components/CourseCard.jsx
@@ -1,7 +1,11 @@
 import cat from '../images/placeholder-image.jpg'
 import '../styles/course-styles.css'
 
-export default function CourseCard() {
+export default function CourseCard ( props ) {
+
+    // Student props
+    const { role, title, description, category, price, rating } = props;
+
 
     const courseTitle = "Javascript for cats"
     const courseImage = cat;
@@ -13,38 +17,26 @@ export default function CourseCard() {
             <div className="container">
                 <div className="course-card">
                     <div>
-                        <img className="course-image" src={courseImage} draggable='false' />
+                        <img className="course-image" src={courseImage} alt={title} draggable='false' />
                         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="three-dots" viewBox="0 0 16 16">
                             <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0" />
                         </svg>
                         <div className="course-content">
                             <div className="course-header">
-                                <div className="course-title">{courseTitle}</div>
-                                <div className="course-educator">Educator - {courseEducator}</div>
+                                <div className="course-title">{title}</div>
                             </div>
-                            <div className="course-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque mi quam, suscipit sit amet euismod vel, imperdiet sed justo. Sed mollis felis velit, ac iaculis quam posuere eu. Nunc dignissim vel urna ut semper. Suspendisse sapien risus, interdum in est sit amet, eleifend tempor.</div>
+                            <div className="course-description">{description}</div>
                             <div className="course-rating">
-                                <div>4.6</div>
+                                <div>{rating}</div>
                                 <div className="stars">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#eab308" className="bi bi-star-fill" viewBox="0 0 16 16">
-                                        <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
-                                    </svg>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#eab308" className="bi bi-star-fill" viewBox="0 0 16 16">
-                                        <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
-                                    </svg>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#eab308" className="bi bi-star-fill" viewBox="0 0 16 16">
-                                        <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
-                                    </svg>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#eab308" className="bi bi-star-fill" viewBox="0 0 16 16">
-                                        <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
-                                    </svg>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-star-fill" viewBox="0 0 16 16">
-                                        <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
-                                    </svg>
+                                    {[...Array(Math.round(rating))].map((_, index) => (
+                                        <svg key={index} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#eab308" className="bi bi-star-fill" viewBox="0 0 16 16">
+                                            <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                                        </svg>
+                                    ))}
                                 </div>
-                                <div className="reviews">(120K reviews)</div>
                             </div>
-                            <div className="course-price">$199.95</div>
+                            <div className="course-price">{role == "Student" && price}</div>
                         </div>
                     </div>
                 </div>
