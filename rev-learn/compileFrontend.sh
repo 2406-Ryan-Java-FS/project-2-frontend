34c04bf834cdf73010d3f419da4b3ef0d4733104
mrCoutureLocal <>
2024-07-29 18:25:12 -0400

    Setup build and upload frontend ec2 script

--- rev-learn/compileFrontend.sh
+++ rev-learn/compileFrontend.sh
@@ -4,6 +4,8 @@
 #nginx can serve the build folder on localhost/project-2
 
 ##WATCH OUT!!! The beginning / must have a space, otherwise this command will append the wrong thing to all the links
-PUBLIC_URL=" /project-2" npx react-scripts build
+# PUBLIC_URL=" /project-2" npx react-scripts build
+
+npx react-scripts build
 
 #Links and Route components change the browser url. They are not what they seem.
