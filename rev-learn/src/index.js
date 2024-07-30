f4b0dba30d6808c08f1cd023966cbfe3ffde2114
Hayoung Cheon <70024528+oyunmintrio95@users.noreply.github.com>
2024-07-30 08:37:17 -0700

    Revert "Course detail"

--- rev-learn/src/index.js
+++ rev-learn/src/index.js
@@ -1,18 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-import AppProvider from "./provider/AppProvider";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AppProvider>
-        <App />
-      </AppProvider>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
+
--- rev-learn/src/pages/course-detail/course-detail-description.jsx
+++
@@ -1,7 +0,0 @@
-export default function CourseDetailDescription({description}){
-
-    return(<div>
-        <h2>Course Description</h2>
-        <p>{description}</p>
-    </div>);
-}
