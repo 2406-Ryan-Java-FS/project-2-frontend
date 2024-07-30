293b5ebfbcf8388d287bc05fbe510b701568a2b0
Scruffy pc <cayabyabrr@yahoo.com>
2024-07-30 09:09:59 -0700

    Reverted index.js

--- rev-learn/src/index.js
+++ rev-learn/src/index.js
@@ -4,15 +4,13 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import AppProvider from "./provider/AppProvider";
-import UserCourseCatalog from "./pages/UserCourseCatalog/UserCourseCatalog";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <AppProvider>
-        {/* <App /> */}
-        <UserCourseCatalog />
+        <App />
       </AppProvider>
     </BrowserRouter>
   </React.StrictMode>
