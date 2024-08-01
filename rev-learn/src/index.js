ee0963c7296cc68061cb1692a32219845a2c79b2
Scruffy pc <cayabyabrr@yahoo.com>
2024-08-01 13:12:17 -0700

    Reverted index.js

--- rev-learn/src/index.js
+++ rev-learn/src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import AppProvider from "./provider/AppProvider";
 
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
