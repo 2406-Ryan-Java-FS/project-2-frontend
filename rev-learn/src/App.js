dc7b82eaa2e468512c7d78951ec247c521a257a7
Eugene Kwaka <kwakaeugene@gmail.com>
2024-08-01 10:57:06 -0500

    Ensured the static sidebar is only displayed on the individual course pages. It won't display on other pages.

--- rev-learn/src/App.js
+++ rev-learn/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {Route,Routes} from 'react-router-dom';
+import {Route,Routes, useLocation} from 'react-router-dom';
 import { BrowserRouter,Link } from 'react-router-dom';
 import HomeComponent from './components/HomeComponent';
 import Register from './components/Register';
@@ -10,26 +10,39 @@ import CourseQuizzes from './pages/individual-course-student/course-quizzes';
 import CourseDiscussions from './pages/individual-course-student/course-discussions';
 import CourseGrades from './pages/individual-course-student/course-grades';
 
-export default function App() {
+
+const AppContent = () => {
+  const location = useLocation();
+  const showSideBar = location.pathname.startsWith('/courses');
+
   return (
-    <div className="App">
-      <BrowserRouter>
-      <SideBar />
+    <div className="App" style={{ display: 'flex' }}>
+      {showSideBar && <SideBar />}
+      <div style={{ marginLeft: showSideBar ? '240px' : '0', padding: '20px', flexGrow: 1 }}>
         <nav id="navbar">
           <Link to="/">Home</Link>{" ~ "}
           <Link to="/login">Login</Link>{" ~ "}
           <Link to="/register">Register</Link>
         </nav>
         <Routes>
-          <Route path='' element={<HomeComponent />} />
-          <Route path='/register' element={<Register/>}/>
-          <Route path='/login' element={<Login/>}/>
+          <Route path="/" element={<HomeComponent />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/courses/:courseId" element={<CourseHome />} />
           <Route path="/courses/:courseId/quizzes" element={<CourseQuizzes />} />
           <Route path="/courses/:courseId/discussions" element={<CourseDiscussions />} />
           <Route path="/courses/:courseId/grades" element={<CourseGrades />} />
         </Routes>
-      </BrowserRouter>
+      </div>
     </div>
   );
+};
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <AppContent />
+    </BrowserRouter>
+  );
 }
+
