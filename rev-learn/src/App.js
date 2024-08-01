453db4bd8d012a9c90463be58ff1bd1b15a8546b
mrCoutureLocal <>
2024-08-01 16:55:01 -0400

    Removed extra logout from the bottom of the navbar

--- rev-learn/src/App.js
+++ rev-learn/src/App.js
@@ -65,7 +65,6 @@ export default function App() {
           <Link to="/course-catalog">Catalog</Link>
           <Link to="/quiz">Quiz</Link>
           <Link to="/edit-question">Edit&nbsp;Question</Link>
-          <Link to="/login">Logout</Link>
         </NavDrawer>
 
 
