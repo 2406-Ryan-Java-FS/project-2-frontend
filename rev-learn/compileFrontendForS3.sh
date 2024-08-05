dac9bd97ea5f51e1abb97fe1ef8210075870be94
mrCoutureLocal <>
2024-08-05 11:42:32 -0400

    Tried to move background images into public folder so they can be fetched during final React build. Could not do it.

--- rev-learn/compileFrontendForS3.sh
+++ rev-learn/compileFrontendForS3.sh
@@ -4,7 +4,6 @@
 #Prepends /s3 to all the urls in the React build so it can fetch it's own files
 ##WATCH OUT!!! The beginning / must have a space, otherwise this command will append the wrong thing to all the links
 
-BUILD_PATH=./s3 PUBLIC_URL=" /s3" npx react-scripts build --output-path=./s3
-
+BUILD_PATH=./s3 PUBLIC_URL=" /s3" npx react-scripts build
 
 #Links and Route components change the browser url. They are not what they seem.
