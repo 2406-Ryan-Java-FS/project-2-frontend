068d42d124d3759f35fc7e774ad540c3b065431f
mrCoutureLocal <>
2024-08-04 20:00:30 -0400

    Looked into adding s3 to the beginning of the React final build url. Can't proxy on aws.

---
+++ rev-learn/compileFrontendForS3.sh
@@ -0,0 +1,10 @@
+
+#nginx can proxy pass localhost/s3 into someones aws S3 bucket
+
+#Prepends /s3 to all the urls in the React build so it can fetch it's own files
+##WATCH OUT!!! The beginning / must have a space, otherwise this command will append the wrong thing to all the links
+
+BUILD_PATH=./s3 PUBLIC_URL=" /s3" npx react-scripts build --output-path=./s3
+
+
+#Links and Route components change the browser url. They are not what they seem.
