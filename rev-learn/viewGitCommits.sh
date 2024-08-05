a09c594fd9c8968980a947225c148ec78577ac87
mrCoutureLocal <>
2024-08-05 14:33:19 -0400

    Adding git log viewing script

---
+++ rev-learn/viewGitCommits.sh
@@ -0,0 +1,3 @@
+
+#Displays time, branch, author, commit message, and files changed recently
+git log --all --name-status --pretty=format:'%C(yellow)%h %C(red)%ad %C(cyan)%d %C(green)%an %C(cyan)%s' --date=format-local:%Y-%m-%d_%H:%M
