ef272cea27b918ac767ef49f233fd5bca776e69d
mrCoutureLocal <>
2024-08-05 14:48:36 -0400

    Added git fetch to script so it's more convenient

--- rev-learn/viewGitCommits.sh
+++ rev-learn/viewGitCommits.sh
@@ -1,3 +1,4 @@
 
 #Displays time, branch, author, commit message, and files changed recently
+git fetch
 git log --all --name-status --pretty=format:'%C(yellow)%h %C(red)%ad %C(cyan)%d %C(green)%an %C(cyan)%s' --date=format-local:%Y-%m-%d_%H:%M
